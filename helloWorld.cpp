aefec46613bf1e732ab46ffb760b8b7e8fa43da4
Gustavo Ferreira <snt.gustavoferreira@gmail.com>
2023-05-13 09:35:12 -0300

    First commit

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+    // Comments!
+    /*
+        Multi-line
+        comment!
+    */
+
+    std::cout << "Hello World!" << '\n';
+    std:: cout << "This is gonna be exciting!" << '\n';
+    return 0;
+}
