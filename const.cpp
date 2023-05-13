427d54fbfaee660440579772550bcfde9515f4df
Gustavo Ferreira <snt.gustavoferreira@gmail.com>
2023-05-13 14:26:56 -0300

    Const

---
+++ const.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    const double PI = 3.14159;
+    double radius = 10;
+    double circumference = 2 * PI * radius;
+
+    cout << "The circumference is: " << circumference << " cm" << '\n';
+
+    // Examples
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
+
+    return 0;
+}
