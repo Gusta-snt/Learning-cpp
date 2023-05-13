c0b2beaae86b449cec19fd4cb3c6cf7978f48b4e
Gustavo Ferreira <snt.gustavoferreira@gmail.com>
2023-05-13 09:50:45 -0300

    Variables

---
+++ variables.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+int main(){
+
+    // Integers
+
+    int x; // Declaration
+    x = 1;
+    int y = 6;
+    int sum = x + y;
+
+    std::cout << x << '\n';
+    std::cout << y << '\n';
+    std::cout << sum << '\n';
+
+    // Double
+
+    double price = 7.59;
+
+    std::cout << price << '\n';
+
+    // Char
+
+    char grade = 'A';
+
+    std::cout << grade << '\n';
+
+    // Boolean
+
+    bool student = true;
+
+    std::cout << student << '\n';
+
+    // String
+
+    std::string name = "Gustavo";
+
+    std::cout << "Hello " << name << '\n';
+
+    return 0;
+}
